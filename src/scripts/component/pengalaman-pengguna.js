04b687bfcdb8693fc9f09f4b563eaeb52ec0090b
Ano <137753056+Graciano24@users.noreply.github.com>
2025-01-19 22:05:43 +0700

    Add files via upload

--- src/scripts/component/pengalaman-pengguna.js
+++ src/scripts/component/pengalaman-pengguna.js
@@ -11,7 +11,7 @@ class PengalamanPengguna extends HTMLElement {
                         <span class="material-symbols-outlined">format_quote</span>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia at tempore sequi cupiditate architecto ea cum dignissimos accusantium aspernatur?</p>
                         <div class="namaAuthor">
-                            <h4>Muhammad Khanafi</h4>
+                            <h1>Muhammad Khanafi</h1>
                             <p>Juara 1 Matematika Internasional</p>
                         </div>
                     </div>
