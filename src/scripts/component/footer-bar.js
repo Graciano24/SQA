05f22bbfa25ab6daae327df89f9ce42e6c0e2e11
Ano <137753056+Graciano24@users.noreply.github.com>
2025-01-19 16:59:17 +0700

    Add files via upload

--- src/scripts/component/footer-bar.js
+++ src/scripts/component/footer-bar.js
@@ -5,46 +5,14 @@ class FooterBar extends HTMLElement {
           <div class="detailFooter">
             <div class="detailInstansi">
               <div class="logo">
-                <img
-                  src="./img/wolf.png"
-                  srcset="./img/wolf-200w.png 200w, ./img/wolf-400w.png 400w, ./img/wolf-600w.png 600w"
-                  sizes="(max-width: 600px) 200px, (max-width: 1200px) 400px, 600px"
-                  width="600"
-                  height="100"
-                  alt="logo"
-                  loading="lazy"
-                >
+                <img src="./img/wolf.png" alt="logo">
                 <p>LoneWolf</p>
               </div>
               <p>Jalan Duri No. 90, Duri Kepa, Kebon Jeruk, Kota Jakarta Barat, DKI Jakarta 20123</p>
               <div class="sosmed">
-                <a href="https://www.instagram.com/dicoding/"><img
-                  src="./img/instagram.png"
-                  srcset="./img/instagram-50w.png 50w, ./img/instagram-100w.png 100w, ./img/instagram-250w.png 250w"
-                  sizes="(max-width: 600px) 50px, (max-width: 1200px) 100px, 250px"
-                  width="250"
-                  height="50"
-                  alt="instagram"
-                  loading="lazy"
-                ></a>
-                <a href="https://www.youtube.com/@DicodingIndonesia"><img
-                  src="./img/youtube.png"
-                  srcset="./img/youtube-50w.png 50w, ./img/youtube-100w.png 100w, ./img/youtube-150w.png 150w"
-                  sizes="(max-width: 600px) 50px, (max-width: 1200px) 100px, 150px"
-                  width="150"
-                  height="50"
-                  alt="youtube"
-                  loading="lazy"
-                ></a>
-                <a href="https://x.com/dicoding"><img
-                  src="./img/twitter.png"
-                  srcset="./img/twitter-50w.png 50w, ./img/twitter-100w.png 100w, ./img/twitter-150w.png 150w"
-                  sizes="(max-width: 600px) 50px, (max-width: 1200px) 100px, 150px"
-                  width="150"
-                  height="50"
-                  alt="twitter"
-                  loading="lazy"
-                ></a>
+                <a href="https://www.instagram.com/dicoding/"><img src="./img/instagram.png" width="250" height="50" alt="instagram" loading="lazy"></a>
+                <a href="https://www.youtube.com/@DicodingIndonesia"><img src="./img/youtube.png" width="100" height="50" alt="youtube" loading="lazy"></a>
+                <a href="https://x.com/dicoding"><img src="./img/twitter.png" width="100" height="50" alt="twitter" loading="lazy"></a>
               </div>
             </div>
             <div class="service" id="service">
@@ -79,3 +47,4 @@ class FooterBar extends HTMLElement {
   }
   
   customElements.define('footer-bar', FooterBar);
+  
