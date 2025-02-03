8c9e5ede60d959e83b92a1f21795466fc0f25eb8
Ano <137753056+Graciano24@users.noreply.github.com>
2025-02-03 17:00:53 +0700

    Add files via upload

--- src/scripts/component/hero.js
+++ src/scripts/component/hero.js
@@ -5,7 +5,7 @@ class HeroElement extends HTMLElement{
                 <div class="descriptionHero">
                     <h1>Awal Masa Depan Mulai dari Pendidikan</h1>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Similique incidunt modi beatae nobis nam quibusdam eligendi aliquid suscipit reprehenderit, numquam minus mollitia nulla commodi veritatis voluptas deserunt atque odio quia!</p>
-                    <a href="#" class="buttonStartStudy">Start Study</a>
+                    <a href="/#pilihanPembelajaran" class="buttonStartStudy">Start Study</a>
                 </div>
                 <img src="./img/gambar-keberhasilan.avif" width="600" height="400" alt="gambar-keberhasilan">
             </div>
